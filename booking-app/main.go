d138ef62c5cba00a18ed2f961624f5e910241c29
Jorge Contti <jcontti@MacBook-Pro-de-Micaela.local>
2022-08-16 11:05:02 -0300

    starting booking-app

---
+++ booking-app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World")
+} // Implement a main function to print a message to the console. A main function executes by default when you run the main package.
